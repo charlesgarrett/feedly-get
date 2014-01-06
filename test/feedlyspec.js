f5057efd9bc75e48bf99e90a25b0481bde23bc03
Charles Garrett <charleslavong@gmail.com>
2014-01-05 20:16:44 -0800

    Using promiscious promises to help avoid some callback crazyness. Also removing api folder for a more flat layout.

--- test/feedlyspec.js
+++ test/feedlyspec.js
@@ -1,76 +1,66 @@
 describe("Behavior contract for the feedly API controller", function() {
 	"use strict";
 	
- 	var feedly, FeedlyAPI = require('../api/feedly').FeedlyAPI;
+ 	var FeedlyAPI = require('../feedly').FeedlyAPI;
 	var querystring = require("querystring");
 
-	var authCode = "AQAA3m97InUiOiIxMTQxNjQ2ODcyMDUzMTE5NzM1NzgiLCJpIjoiNGQ3MjQ3ODctMDNmOC00MGRiLTg0YWEtN2RkNWU4MjFiMjRlIiwicCI6NiwiYSI6IkZlZWRseSBzYW5kYm94IGNsaWVudCIsInQiOjEzODc3NjI1OTIzODB9";
-	var authToken = 'AQAAsfx7ImkiOiI0ZDcyNDc4Ny0wM2Y4LTQwZGItODRhYS03ZGQ1ZTgyMWIyNGUiLCJwIjo2LCJhIjoiRmVlZGx5IHNhbmRib3ggY2xpZW50IiwidCI6MSwidiI6InNhbmRib3giLCJ4Ijoic3RhbmRhcmQiLCJlIjoxMzg4MzY3NDEwNTYzfQ:sandbox';
+	var authCode = "";
 	var server = "http://sandbox.feedly.com";
-	var client_id = "sandbox", client_secret = "QNFQRFCFM1IQCJB367ON";
+	var client_id = "sandbox", client_secret = "";
 	var redirectURI = "http://localhost";
 
-	
-	beforeEach(function(done) {
-		feedly = new FeedlyAPI(authCode, authToken, server, client_id, client_secret, redirectURI);
-		done();
+	var feedly;
+
+	beforeEach(function() {
+		feedly = new FeedlyAPI(authCode, server, client_id, client_secret, redirectURI);
 	});
 	
 	afterEach(function() {
-		//cleanup
+		feedly = null;
 	});
-	
-	it("Verify feedly API object successfully created", function(){
-			expect(feedly).toBeDefined();
-			expect(feedly).toEqual(jasmine.any(FeedlyAPI));
 
-	});
-	
-	//TODO - Priority - Streamline the authentication process - can I avoid using a browser?
-	describe("the authentication methods", function() {
-
-		if(authToken === 'undefined') {
-			
-			it("should support auth token getters & setters", function() {
-				var token = "1234abcd", refreshToken = "1234refresh";
-				expect(feedly.getSavedAuthToken()).not.toBeDefined();
-				feedly.saveToken(token, refreshToken);
-				expect(feedly.getSavedAuthToken()).toBe(token);
-				expect(feedly.getSavedRefreshToken()).toBe(refreshToken);
-			});
+/*	describe("the authentication methods", function() {
 		
-		
-			it("should use the authCode to get an authToken from feedly", function() {
+		it("should use the authCode to get an authToken from feedly", function() {
 				expect(feedly.getSavedAuthToken()).not.toBeDefined();
 			
+				var token = null;
 				runs(function() {
-					feedly.getAuthToken();
+					feedly.init()
+					.then(function() {token = feedly.getSavedAuthToken()});
 				});
-			
+				
 				waitsFor(function() {
-					return feedly.getSavedAuthToken() != 'undefined';
-				}, "The token should be defined", 750);
-			});
-		
-			it("should use its refresh token to request a new auth token", function() {
+					token != null;
+				}, "Auth token should be saved", 2000);
 				
 				runs(function() {
-					feedly.getAuthToken();
-					feedly.refreshAuthToken();
+					expect(feedly.getSavedAuthToken()).toBeDefined();
+				})
+			});
+		
+	   it("should use its refresh token to request a new auth token", function() {
+				
+				var refreshToken = "dummyValue";
+				runs(function() {
+					feedly.init()
+					.then(function() {return feedly.refreshAuthToken() })
+					.then(function() { refreshToken = feedly.getSavedRefreshToken()})
 				});	
 				
 				waitsFor(function() {
-					return feedly.getSavedRefreshToken() == null;
-				}, "The refresh_token should be set to null ", 750);
-			
+					refreshToken == null;
+				}, "the refresh token to be null after it is used to get a new Auth token", 2000);
+				
 				runs(function() {
-					expect(feedly.getSavedAuthToken()).not.toBeNull();
-				});	
-			});
-		}
-		
-	});
+					var newToken = feedly.getSavedAuthToken();
+					expect(newToken).toBeDefined();
+					expect(newToken).not.toBeNull();
+				});
+		}); 
 	
+	}); */
+
 	describe("the feeds methods", function() {
 		
 		it("should deliver JSON data about specific feed", function() {
@@ -78,9 +68,10 @@ describe("Behavior contract for the feedly API controller", function() {
 			var feedInfo = null;
 			
 			runs(function() {
-				feedly.getFeedInfo(feedID, function(returnedInfo) { 
-					feedInfo = returnedInfo;
-				});
+				feedly.getFeedInfo(feedID)
+					.then(function(returnedInfo) { 
+						feedInfo = returnedInfo;
+					});
 			});
 			
 			waitsFor(function() {
@@ -102,12 +93,14 @@ describe("Behavior contract for the feedly API controller", function() {
 	
 	
 	describe("the Stream-Entry methods ", function() {
+		
 		it("should deliver the JSON content for some entry", function() {
 			var entryID = "vSbjObuspiUUUlHx496XW/WaRBw2NaRdTW1NAiwoLAs=_1431c635828:bf50:7cda226";
 			var entry = null;
 			
 			runs(function() {
-				feedly.getEntry(entryID, function(returnedInfo) { entry = returnedInfo});
+				feedly.getEntry(entryID)
+					.then(function(returnedInfo) { entry = returnedInfo});
 			})
 			
 			waitsFor(function() {
@@ -123,36 +116,40 @@ describe("Behavior contract for the feedly API controller", function() {
 			});
 			
 		}); 
-		
+	
 		it("should deliver the entry IDs from a user's stream", function() {
 			var streamId = "vSbjObuspiUUUlHx496XW/WaRBw2NaRdTW1NAiwoLAs=_1431c635828:bf50:7cda226";
 			var id_list = null;
 			
 			runs(function() {
-				feedly.getStreamEntryIds(streamId, function(returnedInfo) { id_list = returnedInfo});
+				feedly.init()
+					.then(function() {return feedly.getStreamEntryIds(streamId)})
+					.then(function(result) { id_list = result});
 			})
 			
 			waitsFor(function() {
 				return id_list != null;
-			}, "list of IDs should be returned", 750);
+			}, "list of IDs should be returned", 2000);
 			
 			
 			runs(function() {
 				expect(id_list.ids).toBeDefined();
 			});
 		});
-		
+			
 		it("should deliver the entry contents from a user's stream", function() {
 			var streamId = "feed/http://www.engadget.com/rss.xml";
 			var contents = null;
 
 			runs(function() {
-				feedly.getStreamContents(streamId, function(returnedInfo) { contents = returnedInfo});
+				feedly.init()
+					.then(function() {return feedly.getStreamContents(streamId)})
+					.then(function(result) { contents = result});
 			})
 			
 			waitsFor(function() {
 				return contents != null;
-			}, "stream contents should be returned", 750);
+			}, "stream contents should be returned", 2000);
 			
 			
 			runs(function() {
@@ -171,12 +168,14 @@ describe("Behavior contract for the feedly API controller", function() {
 			var mix = null;
 			
 			runs(function() {
-				feedly.getStreamMixContents(streamId, function(returnedInfo) { mix = returnedInfo});
+				feedly.init()
+				.then(function() {return feedly.getStreamMixContents(streamId)})
+				.then(function(result) { mix = result});
 			})
 			
 			waitsFor(function() {
 				return mix != null;
-			}, "engaging content should be returned", 750);
+			}, "engaging content should be returned", 2000);
 			
 			runs(function() {
 				expect(mix.title).toBeDefined();
@@ -196,14 +195,16 @@ describe("Behavior contract for the feedly API controller", function() {
 			var userInfo = null;
 			
 			runs(function() {
-				feedly.getUserProfile(function(returnedInfo) { userInfo = returnedInfo});
+				feedly.init()
+					.then(function() {return feedly.getUserProfile()}, function(err) {console.log(err)})
+					.then(function(result) {console.log('result '+result); userInfo = result});
 			});
 			
 			waitsFor(function() {
 				return userInfo != null;
-			}, " userInfo should not be null", 750);
+			}, " user profile should not be null", 2000)
 			
-			runs(function() {
+			runs(function(){
 				if(typeof userInfo.twitterUserId == "undefined") {
 					expect(userInfo.email).toBeDefined();
 					expect(userInfo.familyName).toBeDefined();
@@ -213,21 +214,23 @@ describe("Behavior contract for the feedly API controller", function() {
 					expect(userInfo.twitter).toBeDefined();
 					expect(userInfo.id).toBeDefined();
 					expect(userInfo.client).toBeDefined();		
-				}
+				}	
 			});
 		});
 		
 
-		it("should deliver json user subscriptions", function() {
+		 it("should deliver json user subscriptions", function() {
 			var subs = null;
 			
 			runs(function() {
-				feedly.getUserSubscriptions(function(returnedInfo) { subs = returnedInfo});
+				feedly.init()
+					.then(function() { return feedly.getUserSubscriptions()})
+					.then(function(result) { subs = result});
 			});
 			
 			waitsFor(function() {
 				return subs != null;
-			}, " subscriptions should not be null", 750);
+			}, " subscriptions should not be null", 2000);
 			
 			runs(function() {
 				if(typeof subs[0] === "object") {
@@ -243,14 +246,15 @@ describe("Behavior contract for the feedly API controller", function() {
 			
 		it("should deliver user's subscription categories", function() {
 			var categories = null;
-			
 			runs(function() {
-				feedly.getUserCategories(function(returnedInfo) { categories = returnedInfo});
+				feedly.init()
+					.then(function() {return feedly.getUserCategories()} )
+					.then(function(result) { categories = result} );
 			});
 						
 			waitsFor(function() {
 				return categories != null;	
-			}, "categories should not be null", 750);
+			}, "categories should not be null", 2000);
 			
 			runs(function() {
 				if(categories.length > 0) {
@@ -265,12 +269,14 @@ describe("Behavior contract for the feedly API controller", function() {
 			var tags = null;
 			
 			runs(function() {
-				feedly.getUserTags(function(returnedInfo) { tags = returnedInfo});
+				feedly.init()
+					.then(function() {return feedly.getUserTags()})
+					.then(function(result) { tags = result});
 			});
 			
 			waitsFor(function() {
 				return tags != null;
-			}, "tags should not be null", 750);
+			}, "tags should not be null", 2000);
 			
 			runs(function() {
 				if(tags.length > 0) {
@@ -279,20 +285,29 @@ describe("Behavior contract for the feedly API controller", function() {
 			});
 		});
 		
+		
 	}); 
 	
 	describe("the search API methods", function() { 
+		var feedly;
+
+		beforeEach(function() {
+			feedly = new FeedlyAPI(authCode, server, client_id, client_secret, redirectURI);
+		});
+		
 		it("should return a list of feeds that match a keyword", function() {
 			var feeds = null;
 			var keyword = "apartment therapy";
 			
 			runs(function() {
-				feedly.searchFeeds(keyword, function(returnedInfo) { feeds = returnedInfo});
+				feedly.init()
+					.then(function() {return feedly.searchFeeds(keyword)} )
+					.then(function(result) { feeds = result}, function(err) {console.log(err)});
 			});
 			
 			waitsFor(function() {
 				return feeds != null;
-			}, "feeds should not be null", 750);
+			}, "feeds should not be null", 2000);
 			
 			runs(function() {
 				expect(feeds.results).toBeDefined();
@@ -306,5 +321,6 @@ describe("Behavior contract for the feedly API controller", function() {
 				expect(feeds.results[0].lastUpdated).toBeDefined();
 			});
 		});
-	});
+		
+	}); 
 });
